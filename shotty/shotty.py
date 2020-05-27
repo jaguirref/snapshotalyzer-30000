7b98a0113fcba33b963754f90cb67276ccd64aa1
Jorge Aguirre <JAF@Jorges-MacBook-Pro.local>
2020-05-27 17:50:59 +0200

    list --all snapshot option

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -27,8 +27,10 @@ def snapshots():
 
 @snapshots.command('list')
 @click.option('--project', default=None,
-            help="Only snapshots for project (tag Project:<name>)")
-def list_snapshots(project):
+    help="Only snapshots for project (tag Project:<name>)")
+@click.option('--all', 'list_all', default=False, is_flag=True,
+    help="List all snapshots for each volume, not just the most recent")            
+def list_snapshots(project, list_all):
     "List EC2 snapshots"
 
     instances = filter_instances(project)
@@ -40,11 +42,12 @@ def list_snapshots(project):
                     s.id,
                     v.id,
                     i.id,
-                    s.tate,
+                    s.state,
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
-    
+                if s.state == 'completed' and not list_all: break
+
     return
 
 @cli.group('volumes')
